4af0e26ce3bc035c1710142a87585a8bb30229d0
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2022-02-16 16:47:14 +0000

    Add some documentation

--- src/scheduino/gpio.zig
+++ src/scheduino/gpio.zig
@@ -4,28 +4,39 @@ const utils = @import("../libz/libz.zig").Utilities;
 // Lock on all the GPIO pins
 var Lock: [24]LockState = [_]LockState{.Unlocked} ** 24;
 
+// Possible states of a lock
 const LockState = union(enum) {
     Unlocked,
     Locked: u8,
 };
 
+/// Acquire the lock on a GPIO pin. Only one process can acquire
+/// it at a time
 pub fn acquireLock(id: u8) bool {
     utils.fence();
     var locked = Lock[id];
-    switch (locked) {
+    var result = switch (locked) {
         .Unlocked => {
             Lock[id] = LockState{ .Locked = scheduler.currentId };
-            return true;
+            break true;
         },
         .Locked => |lock_id| {
             // WARN: Maybe disallow acquiring the same pin multiple times
             // WARN: from a unique process
-            return (lock_id == scheduler.currentId);
+            break (lock_id == scheduler.currentId);
         },
-    }
+    };
     utils.deFence();
+    return result;
 }
 
+pub fn acquireLockBlocking(id: u8) void {
+    while (!acquireLock(id)) {
+        asm volatile ("nop" ::: "memory");
+    }
+}
+
+/// Release the lock on a GPIO pin.
 pub fn releaseLock(id: u8) void {
     utils.fence();
     var locked = Lock[id];
--- src/scheduino/process.zig
+++ src/scheduino/process.zig
@@ -3,6 +3,14 @@ const memory = @import("memory.zig");
 const scheduler = @import("scheduler.zig");
 const Libz = @import("../libz/libz.zig");
 
+/// OS representation of a process
+///
+/// * pid: id of the process
+/// * func: start function
+/// * state: current state
+/// * wait_offset: if in Waiting mode, number of cycles to wait for
+/// * stack_pointer: saved SP
+/// * stack_layout: comptime information about the process's stack
 pub const Process = struct {
     pid: u8,
     func: fn () void,
@@ -12,11 +20,21 @@ pub const Process = struct {
     stack_layout: memory.StackLayout,
 };
 
+/// * Comptime definition of a process
+///
+/// * func: start function
+/// * stack_size: requested stack size
 pub const ProcDef = struct {
     func: fn () void,
     stack_size: memory.StackSize,
 };
 
+/// State of a process
+///
+/// * New: Not yet started
+/// * Running: normal mode
+/// * Waiting: paused, waiting for an event
+/// * Dead: will not be revived
 pub const ProcState = enum(u8) {
     New,
     Running,
@@ -24,10 +42,13 @@ pub const ProcState = enum(u8) {
     Dead,
 };
 
+/// Cooperatively hand control over to the next process
+/// WARN: Not workign currently
 pub fn handOver() void {
     scheduler.handProcessOver();
 }
 
+/// Initialize sleep
 pub fn sleepTick(tick: usize) void {
     var self_proc = &scheduler.MemState.processes[scheduler.currentId];
     self_proc.state = .Waiting;
@@ -37,6 +58,7 @@ pub fn sleepTick(tick: usize) void {
 
 pub export var k = false;
 
+/// Test process n.1
 pub fn mainProcess() void {
     scheduler.hasJumped = true;
 
@@ -53,6 +75,7 @@ pub fn mainProcess() void {
     }
 }
 
+/// Test process n.2
 pub fn secondProcess() void {
     var st: bool = true;
     Libz.GpIO.DIGITAL_MODE(6, .OUTPUT) catch {};
